e9581c9d644c7a74dce48c58475a8381d96af452
Bilata Wodisha <12mastwal@gmail.com>
2023-02-23 19:57:34 +0300

    More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - outputs largest prime factor.
+ * Return: It returns 0.
+ */
+
+int main(void)
+{
+	long int n, fp;
+
+	n = 612852475143;
+	for (fp = 2; fp <= n; fp++)
+	{
+		if (n % fp == 0)
+		{
+			n /= fp;
+			fp--;
+		}
+	}
+	printf("%ld\n", fp);
+	return (0);
+}
